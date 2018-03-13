b995fc54a9d6442bc55ea9b6a9e10fa941b974e2
Daniel Terrones <Dterrones444@gmail.com>
2018-03-13 14:43:25 -0700

    added placehodler userSchema

--- model.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-
-const User = mongoose.model('User', userSchema, User);
-
-module.exports = {User};
--- models.js
+++ models.js
@@ -3,6 +3,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+const userSchema = mongoose.Schema({
+    name: String,
+});
 
 const User = mongoose.model('User', userSchema, User);
 
